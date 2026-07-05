355762074b96eeebf2855959abc3818cb14db0a8
yeahgor <yeahgorq@gmail.com>
2026-07-05 13:33:04 +0300

    refactor: added file_id column; added index for selecting by file_id

--- migrations/data/000001_create_chunk_data.up.sql
+++ migrations/data/000001_create_chunk_data.up.sql
@@ -2,11 +2,13 @@ CREATE SCHEMA IF NOT EXISTS cipher;
 
 CREATE TABLE IF NOT EXISTS cipher.chunk_data (
     uuid       UUID        NOT NULL,
+    file_id    UUID        NOT NULL,
     version    INTEGER     NOT NULL,
     ciphertext BYTEA       NOT NULL,
     nonce      BYTEA       NOT NULL,
     created_at TIMESTAMPTZ NOT NULL DEFAULT now(),
-    PRIMARY KEY (uuid, version) -- если прилетает запись с одинаковыми айди и версией - это ошибка (дубликат)
+    PRIMARY KEY (uuid, version)
 );
 
 CREATE INDEX IF NOT EXISTS idx_chunk_data_uuid_version ON cipher.chunk_data (uuid, version DESC);
+CREATE INDEX IF NOT EXISTS idx_chunk_data_file_id_uuid ON cipher.chunk_data (file_id, uuid);
