5e045efb3c456b899e44a3bc34aadf055921e73a
yeahgor <yeahgorq@gmail.com>
2026-07-08 15:58:47 +0300

    feat: added decipher rotation, flusher, cursor settings

--- internal/config/config.go
+++ internal/config/config.go
@@ -2,15 +2,48 @@ package config
 
 import (
 	"fmt"
+	"time"
 
 	"github.com/caarlos0/env/v11"
 )
 
 type Config struct {
-	Logger Logger
-	Server Server `envPrefix:"SERVER_"`
-	KeyDB  DB     `envPrefix:"KEY_DB_"`
-	DataDB DB     `envPrefix:"DATA_DB_"`
+	Logger   Logger
+	Server   Server   `envPrefix:"SERVER_"`
+	KeyDB    DB       `envPrefix:"KEY_DB_"`
+	DataDB   DB       `envPrefix:"DATA_DB_"`
+	Redis    Redis    `envPrefix:"REDIS_"`
+	Flusher  Flusher  `envPrefix:"FLUSHER_"`
+	Rotation Rotation `envPrefix:"ROTATION_"`
+	Decipher Decipher `envPrefix:"DECIPHER_"`
+}
+
+type Redis struct {
+	Addr      string        `env:"ADDR" envDefault:"localhost:6379"`
+	Password  string        `env:"PASSWORD"`
+	DB        int           `env:"DB" envDefault:"0"`
+	CursorTTL time.Duration `env:"CURSOR_TTL" envDefault:"24h"`
+}
+
+type Flusher struct {
+	BatchSize            int           `env:"BATCH_SIZE" envDefault:"1000"`
+	FlushTime            time.Duration `env:"FLUSH_TIME" envDefault:"200ms"`
+	ShutdownFlushTimeout time.Duration `env:"SHUTDOWN_FLUSH_TIMEOUT" envDefault:"5s"`
+	WriteRetries         int           `env:"WRITE_RETRIES" envDefault:"3"`
+	WriteRetryBackoff    time.Duration `env:"WRITE_RETRY_BACKOFF" envDefault:"100ms"`
+}
+
+type Rotation struct {
+	PageSize      int           `env:"PAGE_SIZE" envDefault:"1000"`
+	Workers       int           `env:"WORKERS" envDefault:"8"`
+	RetryAttempts int           `env:"RETRY_ATTEMPTS" envDefault:"3"`
+	RetryBackoff  time.Duration `env:"RETRY_BACKOFF" envDefault:"100ms"`
+}
+
+type Decipher struct {
+	PageSize      int           `env:"PAGE_SIZE" envDefault:"1000"`
+	RetryAttempts int           `env:"RETRY_ATTEMPTS" envDefault:"3"`
+	RetryBackoff  time.Duration `env:"RETRY_BACKOFF" envDefault:"100ms"`
 }
 
 type DB struct {
